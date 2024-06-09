209c7881fc5ed0df965c9f19cb68f785f4723cf4
su1jun <qaxsd530@gmail.com>
2024-06-09 13:08:39 +0900

    240608-1S신입사원

--- 2024/06/1S_1946.py
+++ 2024/06/1S_1946.py
@@ -3,17 +3,21 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     N = int(input())
-    scores = [tuple(map(int, input().split())) for _ in range(N)]
+    dp = [0] * (N + 1)
+    ans = 1
 
-    scores.sort()
-    max_score = scores[0][1]
-    ans = 0
+    for _ in range(N):
+        T1, T2 = map(int, input().split())
+        dp[T1] = T2
 
-    for i in range(1, N):
-        if max_score >= scores[i][1]:
+    min_val = dp[1]
+    for i in range(2, N+1):
+        if dp[i] < min_val:
+            min_val = dp[i]
             ans += 1
 
-        elif scores[i][0] <= max_score < scores[i][1]:
-            max_score = scores[i][1]
-    
     print(ans)
+
+# 0 4 3 2 1 5
+
+# 0 4 5 6 2 7 1 3
